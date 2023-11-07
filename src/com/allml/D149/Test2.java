445ec2f6352f11cfeff7fafbeb2a0a4f8fbffbaf
阿良良木亮 <1051322819@qq.com>
2023-11-07 21:53:55 +0800

    D149

---
+++ src/com/allml/D149/Test2.java
@@ -0,0 +1,29 @@
+package com.allml.D149;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        Collections.addAll(names, "张三丰", "张无忌", "赵敏", "小昭", "张三", "杨过");
+        Stream<String> stream = names.stream();
+
+        Set<String> set = new HashSet<>();
+        Collections.addAll(set, "黑色素", "事实上", "试试看的", "奥兰多", "舒服的", "撒全球");
+        Stream<String> stream1 = set.stream();
+        stream1.filter(s->s.contains("的")).forEach(s -> System.out.println(s));
+
+
+        Map<String,Double> map = new HashMap<>();
+        map.put("van",188.8);
+        map.put("bili",187.0);
+        map.put("monan",185.4);
+        map.put("boy",175.1);
+        map.put("banana",190.4);
+        Stream.of(map);
+
+        String[] names2 = {"的方式","的萨芬","个别地方","阿斯顿","饿啊分","阿三的","和广泛的"};
+
+    }
+}
